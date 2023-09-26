f119757bae4746f506e87dd01bb307672889ff77
Thiago Bastos <thiago.bastos@mira.eti.br>
2023-09-25 22:45:08 -0300

    Aplicação de baralhos na mesa e funcionalidades

--- content/js/jogos.js
+++ content/js/jogos.js
@@ -1,18 +1,32 @@
 const poquer = (() => {
     let cartasDescartadas = [];
-    
+    let cartasMesa = [];
+    let mesaPoquer = mesa();
+    let montanteApostas = 0;
+
     iniciaRodada();
+    adicionaCartaMesa();
+    adicionaCartaMesa();
+    adicionaCartaMesa();
+    adicionaCartaMesa();
+    adicionaCartaMesa();
+    mesaPoquer.atualizaDescarte(cartasDescartadas.length);
 
     function iniciaRodada() {
-        let mesaPoquer = mesa();
-
         cartasDescartadas = [];
         mesaPoquer.montarBaralho(1, ["JOKER"]);
         mesaPoquer.criarJogadores(4);
         mesaPoquer.embaralharCartas(mesaPoquer.verificarBaralho());
         mesaPoquer.darCartasInicio(2);
-        console.log("Baralho:", mesaPoquer.verificarBaralho());
         console.log("Jogadores:", mesaPoquer.verificarJogadores());
     }
+
+    function adicionaCartaMesa() {
+        let carta = mesaPoquer.retirarCartaBaralho();
+        mesaPoquer.atualizaBaralho();
+        cartasMesa.push(carta);
+        mesaPoquer.atualizaMesa(cartasMesa, `<p style="color: white">O montante atual das apostas é: <b style="font-size: 20px">${montanteApostas}</b></p>`);
+    }
+
 
 })();
--- content/js/montaMesa.js
+++ content/js/montaMesa.js
@@ -3,48 +3,48 @@ const mesa = (() => {
     const naipes = ["P", "C", "E", "O"];
     let jogadores = [];
     let baralhoJogo = [];
-    
+
     function montarBaralho(numeroBaralhos, cartasExcluidas, cartasBaralho = baralhoTradicional) {
         cartasExcluidas.map(carta => {
             let posicaoCarta = cartasBaralho.indexOf(carta);
             cartasBaralho.splice(posicaoCarta, 1);
         });
-    
-        for(let x = 0; x < numeroBaralhos; x++) {
+
+        for (let x = 0; x < numeroBaralhos; x++) {
             cartasBaralho.map(carta => {
                 naipes.map(naipe => {
-                    if(carta == "JOKER")
+                    if (carta == "JOKER")
                         baralhoJogo.push(carta)
                     else
-                        baralhoJogo.push(carta + "-" + naipe); 
+                        baralhoJogo.push(carta + "-" + naipe);
                 });
             });
         }
 
         return baralhoJogo;
     }
-    
+
     function criarJogadores(numeroJogadores) {
         jogadores = [];
-        for(let x = 0; x < numeroJogadores; x++){
+        for (let x = 0; x < numeroJogadores; x++) {
             let jogador = {
-                nome: "Jogador - " + (x+1),
+                nome: "Jogador - " + (x + 1),
                 cartas: []
             }
             jogadores.push(jogador);
         }
     }
-    
+
     function embaralharCartas(cartas) {
         for (let i = cartas.length - 1; i > 0; i--) {
-          const sorteador = Math.floor(Math.random() * (i + 1));
-          [cartas[i], cartas[sorteador]] = [cartas[sorteador], cartas[i]];
+            const sorteador = Math.floor(Math.random() * (i + 1));
+            [cartas[i], cartas[sorteador]] = [cartas[sorteador], cartas[i]];
         }
     }
-    
+
     function darCartasInicio(numeroCartas) {
         jogadores.map(jogador => {
-            for(let x = 0; x < numeroCartas; x++) {
+            for (let x = 0; x < numeroCartas; x++) {
                 jogador.cartas.push(retirarCartaBaralho());
             }
         });
@@ -52,10 +52,12 @@ const mesa = (() => {
         let boxJogadores = '';
         jogadores.map((jogador, index) => {
             boxJogadores += `<div class="jogador">
-            <h4 class="nomeJogador">Jogador - ${index}</h4>
+            <h4 class="nomeJogador">Jogador - ${index + 1}</h4>
             <div class="cartas">
-                <div class="carta">${jogador.cartas[0]}</div>
-                <div class="carta">${jogador.cartas[1]}</div>
+                ${jogador.cartas.map((item) => {
+                return `<div class="carta">${item}</div>`
+            })
+                }
             </div>
             <div class="montante">
                 <p class="total"><b>Total:</b> 400</p>
@@ -70,18 +72,42 @@ const mesa = (() => {
         let carta = baralhoJogo[0];
         baralhoJogo.splice(0, 1);
         return carta;
+    
+    }
+    function atualizaBaralho() {
+        $('#cartasBaralho').html(`<p>${baralhoJogo.length}</p>`);
+    }
+
+    function atualizaMesa(cartasMesa, conteudoEspecifico) {
+        let mesa = `<div id="boxMesa">
+            <div class="cartas">
+                ${cartasMesa.map((item) => {
+                    return `<div class="carta">${item}</div>`
+                })
+                    }
+            </div>
+            <div id="conteudoEspecifico">
+                ${conteudoEspecifico}
+            </div>
+        </div>`;
+
+        $('#cartasMesa').html(mesa);
+    }
+
+    function atualizaDescarte(numeroCartasDescartadas) {
+        $('#cartasDescartadas').html(`<p>${numeroCartasDescartadas}</p>`);
     }
 
     function darCartaJogador(jogador, carta) {
         jogadores[jogador].cartas.push(carta);
     }
-    
+
     function retirarCartaJogador(jogador, cartaRetirada) {
-            let posicaoCarta = jogadores[jogador].cartas.indexOf(cartaRetirada);
-            jogadores[jogador].cartas.splice(posicaoCarta, 1);
-            console.log("Carta retirada -", cartaRetirada);
+        let posicaoCarta = jogadores[jogador].cartas.indexOf(cartaRetirada);
+        jogadores[jogador].cartas.splice(posicaoCarta, 1);
+        console.log("Carta retirada -", cartaRetirada);
     }
-    
+
     function verificarBaralho() {
         return baralhoJogo;
     }
@@ -96,6 +122,9 @@ const mesa = (() => {
         embaralharCartas: embaralharCartas,
         darCartasInicio: darCartasInicio,
         retirarCartaBaralho: retirarCartaBaralho,
+        atualizaBaralho: atualizaBaralho,
+        atualizaMesa: atualizaMesa,
+        atualizaDescarte: atualizaDescarte,
         darCartaJogador: darCartaJogador,
         retirarCartaJogador: retirarCartaJogador,
         verificarBaralho: verificarBaralho,
